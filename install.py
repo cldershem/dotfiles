f79cec8a6096d96b54bfd6a7259d759e61df4e5e
Cameron Dershem <cldershem@gmail.com>
2014-01-28 19:28:30 -0500

    hopefully fixed mv bug

--- install.py
+++ install.py
@@ -59,23 +59,23 @@ def get_submodules():
 
 def mv_old_dots():
     old_dir = '{}.dotfiles.old/'.format(home_dir)
+
     if os.path.exists(old_dir):
         command = 'sudo rm -r {}'.format(old_dir)
-        # subprocess.call(command.split())
+        subprocess.call(command.split())
+
     command = 'mkdir {}'.format(old_dir)
-    # subprocess.call(command.split())
+    subprocess.call(command.split())
 
     for dot in dots:
         old_dot = ('{}.{}').format(home_dir, dot)
-        print old_dot
         if os.path.exists(old_dot):
     #         # if os.path.isfile(old_dot):
     #             # dot = '{}.{}'.format(home_dir, dot)
     #         # else:
     #             # dot = '{}.{}/'.format(home_dir, dot)
             command = 'mv {} {}'.format(old_dot, old_dir)
-            print command
-    #         subprocess.call(command.split())
+            subprocess.call(command.split())
             print("Moved {}").format(dot)
         else:
             print("{} doesn't exist.").format(old_dot)
